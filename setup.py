5ea7c25fb1ca7f48ee1f6b4e36165ac047b9c959
Michael Vinyard <47393421+mvinyard@users.noreply.github.com>
2022-06-18 13:13:14 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,20 +4,16 @@ import os
 import sys
 
 setup(
-    name='scdiffeq',
-    python_requires='>3.6.0',
+    name='LARRY-data',
+    python_requires='>3.8.0',
     author='Michael E. Vinyard - Harvard University - Massachussetts General Hospital - Broad Institute of MIT and Harvard',
     author_email='mvinyard@broadinstitute.org',
-    url = 'https://github.com/pinellolab/sc-neural-ODEs',
+    url = 'https://github.com/pinellolab/LARRY-data',
     long_description = open("README.md", encoding="utf-8").read(),
     long_description_content_type = 'text/markdown',
-    description='scdiffeq: single cell dynamics using neural ODEs and variations thereof.',
+    description='LARRY Dataset: lineage and RNA recovery',
     packages = [
-        'scdiffeq',
-        'scdiffeq.tools',
-        'scdiffeq.plotting',
-        'scdiffeq.utilities',
-	'scdiffeq.data',
+        'larry',
     ],
     install_requires=[
         "anndata>=0.7.1",
@@ -25,9 +21,6 @@ setup(
         "torch>=1.1.0",
         "numpy>=1.19.2",
         "pandas>=1.1.2",
-        "torchdiffeq>=0.2.1",
-	"scprep>=1.1.0",
-	"nb-black>=1.0.7",
     ],
     classifiers = [
         "Development Status :: 2 - Pre-Alpha",
