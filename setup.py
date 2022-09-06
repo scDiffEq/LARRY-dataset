4cf24bd213cb4502998a6ef4b8229f0a2cebdd16
Michael Vinyard <47393421+mvinyard@users.noreply.github.com>
2022-09-06 16:05:42 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     python_requires='>3.7.0',
     author='Michael E. Vinyard - Harvard University - Massachussetts General Hospital - Broad Institute of MIT and Harvard',
     author_email='mvinyard@broadinstitute.org',
-    url = 'https://github.com/pinellolab/LARRY-data',
+    url = 'https://github.com/pinellolab/LARRY-dataset',
     long_description = open("README.md", encoding="utf-8").read(),
     long_description_content_type = 'text/markdown',
     description='LARRY Dataset: lineage and RNA recovery',
