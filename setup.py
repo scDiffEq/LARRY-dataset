545a2014b66cea6ebef2ea5ace3af3dc71b51bcc
Michael Vinyard <47393421+mvinyard@users.noreply.github.com>
2022-09-06 15:52:09 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import sys
 
 setup(
     name='LARRY-dataset',
-    version=0.0.0,
+    version='0.0.0',
     python_requires='>3.7.0',
     author='Michael E. Vinyard - Harvard University - Massachussetts General Hospital - Broad Institute of MIT and Harvard',
     author_email='mvinyard@broadinstitute.org',
