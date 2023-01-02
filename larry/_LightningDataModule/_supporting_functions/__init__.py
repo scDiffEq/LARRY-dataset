0b5b98f23db3faf02dd0e5570a227d30f4174816
mvinyard <mvinyard@broadinstitute.org>
2023-01-02 12:44:19 -0700

    clean slate for new lt funcs utils

--- larry/_LightningDataModule/_supporting_functions/__init__.py
+++
@@ -1,7 +0,0 @@
-
-# from ...._io._read_h5ad import _read_h5ad as read_h5ad
-# from ._download_LARRY_in_vitro import _download_LARRY_in_vitro as download_LARRY_in_vitro
-from ._adata_formatting import _format_X as format_X
-from ._split_training_data import _split_training_data as split_training_data
-# from ._annotate_task_groups import _annotate_task_groups as annotate_task_groups
-from ._format_time import _format_time as format_time
--- larry/__init__.py
+++ larry/__init__.py
@@ -1,18 +1,3 @@
+# lt funcs init
 
-__module_name__ = "__init__.py"
-__author__ = ", ".join(["Michael E. Vinyard"])
-__email__ = ", ".join(["vinyard@g.harvard.edu",])
-
-
-# -----------------------------------------------------------------------------
-__version__ = "0.0.1"
-
-
-# -----------------------------------------------------------------------------
-# from ._LightningDataModule._LARRY_LightningDataModule import LARRY_LightningDataModule
-from ._fetch._fetch_data_from_github import _fetch_data_from_github as fetch
-from ._in_vitro import _in_vitro as in_vitro
-
-# -----------------------------------------------------------------------------
-from . import _preprocess as pp
-from . import _analysis as an
+from . import lineage_tracing_funcs as lt
