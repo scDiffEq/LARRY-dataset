747b3f6b38523cbb187e8f3e96b1e61833ba91a6
Michael Vinyard <47393421+mvinyard@users.noreply.github.com>
2023-03-03 12:13:20 -0700

    Update conf.py
    Add napoleon extension

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,7 +34,13 @@ release = '0.0.16'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 
-extensions = ["myst_parser"]
+extensions = [
+    'myst_parser',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.intersphinx',
+]
+
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
