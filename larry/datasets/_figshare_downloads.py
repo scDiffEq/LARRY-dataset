b076b69bc8eb937524164f32b012ef26a55b7c9f
mvinyard <mvinyard@broadinstitute.org>
2023-07-17 12:27:16 -0600

    add minimal docs related to #4

--- larry/datasets/_figshare_downloads.py
+++ larry/datasets/_figshare_downloads.py
@@ -1,12 +1,31 @@
 
+# -- import local dependencies: ------------------------------------------------
 from ._figshare_downloader import FigshareDownloader
 
+
+# -- import packages: ----------------------------------------------------------
+import anndata
+import pathlib
+
+
+# -- set typing: ---------------------------------------------------------------
+from typing import Optional, Union
+
+
+# -- API-facing function: ------------------------------------------------------
 def LARRY_task_one(
-    
-fpath="scdiffeq_data/adata.Weinreb2020.in_vitro.task_01.timepoint_recovery.h5ad",
-    url="https://figshare.com/ndownloader/files/41576409",
-):
+    fpath: Optional[Union[str, pathlib.Path]] = None,
+    url: Optional[Union[str, pathlib.Path]] = None,
+) -> anndata.AnnData:
     """Downloader for Timepoint Recovery Task AnnData"""
+
+    if fpath is None:
+        fpath = (
+            "scdiffeq_data/adata.Weinreb2020.in_vitro.task_01.timepoint_recovery.h5ad"
+        )
+
+    if url is None:
+        url = "https://figshare.com/ndownloader/files/41576409"
 
     figshare_downloader = FigshareDownloader()
     return figshare_downloader(url=url, fpath=fpath)
