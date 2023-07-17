207f00a36fd6cf51427e3b41f3db4124e014ce64
mvinyard <mvinyard@broadinstitute.org>
2023-07-17 12:07:24 -0600

    bug fixes related to #4

---
+++ larry/utils/_info_message.py
@@ -0,0 +1,58 @@
+
+# -- import packages: ----------------------------------------------------------
+import licorice_font
+
+
+# -- set typing: ---------------------------------------------------------------
+NoneType = type(None)
+
+
+class InfoMessage:
+    def __init__(
+        self,
+        silent: bool = False,
+        INFO: str = "INFO",
+        color: str = "BLUE"
+    ) -> None:
+
+        """
+
+        Parameters:
+        -----------
+        silent
+            type: bool
+            default: False
+
+        INFO
+            type: str
+            default: "INFO"
+
+        color
+            type: str
+            default: "BLUE"
+
+        Returns:
+        --------
+        None
+        """
+
+        self.INFO = licorice_font.font_format(INFO, [color])
+        self._SILENT = silent
+
+    def _format_msg_title(self, INFO) -> str:
+        return f" - [{INFO}] | "
+
+    def __call__(self, msg: str) -> NoneType:
+
+        """
+        Prints message according to above-specified formatting.
+
+        Parameters:
+        -----------
+        msg
+            type: str
+        """
+
+        if not self._SILENT:
+            self.BASE = self._format_msg_title(self.INFO)
+            print(self.BASE + msg)
