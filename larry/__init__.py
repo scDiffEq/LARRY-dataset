aeeac479874bfdd05d3036eecebb29616630344c
Michael Vinyard <mvinyard.ai@gmail.com>
2025-05-18 14:45:00 -0600

    update callbacks

--- larry/__init__.py
+++ larry/__init__.py
@@ -1,3 +1,5 @@
+from .__version__ import __version__
+
 from . import _analysis as an
 from . import _plotting as pl
 from . import _tools as tl
@@ -7,4 +9,4 @@ from . import utils
 from . import tasks
 
 
-__all__ = ["an", "pl", "tl", "callbacks", "datasets", "utils", "tasks"]
+__all__ = ["an", "pl", "tl", "callbacks", "datasets", "utils", "tasks", "__version__"]
