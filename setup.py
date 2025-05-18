719df37466151738a9110038e40d93343e4a3014
Michael Vinyard <47393421+mvinyard@users.noreply.github.com>
2025-05-18 12:21:03 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
+# -- import packages: ---------------------------------------------------------
 import setuptools
 import re
 import os
 import sys
 
+# -- fetch requirements packages: ---------------------------------------------
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+with open(f"larry/__version__.py") as v:
+    exec(v.read())
+
+# -- run setup: ---------------------------------------------------------------
 setuptools.setup(
     name='LARRY-dataset',
     version='0.0.2rc1',
-    python_requires='>3.9.0',
+    python_requires='>3.10',
     author='Michael E. Vinyard',
     author_email='mvinyard.ai@gmail.com',
     url = 'https://github.com/scDiffEq/LARRY-dataset',
@@ -15,14 +24,10 @@ setuptools.setup(
     description='LARRY Dataset: lineage and RNA recovery',
     packages = setuptools.find_packages(),
     include_package_data=True,
-    install_requires=[
-        "anndata>=0.9.1",
-  #      "pytorch-lightning>=2.0.2",
-   #     "wget>=3.2",
-    ],
+    install_requires=requirements,
     classifiers = [
         "Development Status :: 2 - Pre-Alpha",
-        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Intended Audience :: Science/Research",
         "Topic :: Scientific/Engineering :: Bio-Informatics",
     ],
